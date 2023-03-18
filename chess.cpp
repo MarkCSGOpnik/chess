f60fbd5855dbbe0241b738203163b9be3acc87e3
user <schwenkmark2007@gmail.com>
2023-03-18 18:27:37 +0600

    Добавьте файлы проекта.

---
+++ chess.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int whiteorblack()
+{
+	srand(time(NULL));
+	int whiteorblack = rand() % 2;
+	return whiteorblack;
+}
+int game()
+{
+	
+}
+int main()
+{
+
+}
